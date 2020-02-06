739cc1d98c8514b906ce003fb2af19f4ad873aae
Cain <caintaylor2015@gmail.com>
2020-02-06 14:50:10 -0600

    Tic Tac Toe before debugging/Unit Testing.

---
+++ TicTacToe/TicTacToeBLL/GameBoard.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeBLL
+{
+    public class GameBoard
+    {
+        //NW = 0,0 - N = 0,1 - NE = 0,2
+        //W  = 1,0 - C = 1,1 - E  = 1,2
+        //SW = 2,0 - S = 2,1 - SE = 2,2
+
+        private char[,] playTable = new char[3, 3];
+        Random Players = new Random();
+        private string player1;
+        private string player2;
+        public int turnCounter;
+
+        public void SetBoard()
+        {
+            playTable[0, 0] = ' ';
+            playTable[0, 1] = ' ';
+            playTable[0, 2] = ' ';
+            playTable[1, 0] = ' ';
+            playTable[1, 1] = ' ';
+            playTable[1, 2] = ' ';
+            playTable[2, 0] = ' ';
+            playTable[2, 1] = ' ';
+            playTable[2, 2] = ' ';
+        }
+
+        public bool AssignBoard(int x, string y)
+        {
+            bool isAvailable = false;
+            int left = 0;
+            int right = 0;
+            char choice;
+
+            switch (x)
+            {
+                case 1:
+                    left = 0;
+                    right = 0;
+                    break;
+                case 2:
+                    left = 0;
+                    right = 1;
+                    break;
+                case 3:
+                    left = 0;
+                    right = 2;
+                    break;
+                case 4:
+                    left = 1;
+                    right = 0;
+                    break;
+                case 5:
+                    left = 1;
+                    right = 1;
+                    break;
+                case 6:
+                    left = 1;
+                    right = 2;
+                    break;
+                case 7:
+                    left = 2;
+                    right = 0;
+                    break;
+                case 8:
+                    left = 2;
+                    right = 1;
+                    break;
+                case 9:
+                    left = 2;
+                    right = 2;
+                    break;
+                default:
+                    break;
+            }
+
+            if (playTable[left, right] == ' ')
+            {
+                isAvailable = true;
+                if (y == "player1")
+                {
+                    choice = 'O';
+                }
+
+                else
+                {
+                    choice = 'X';
+                }
+                playTable[left, right] = choice;
+                turnCounter++;
+            }
+            return isAvailable;
+        }
+
+        public char readTable(int x, int y)
+        {
+            char value = playTable[x, y];
+            return value;
+        }
+
+        public bool checkWin()
+        {
+            bool isOver = false;
+
+            if (readTable(0, 0) == readTable(0, 1) && readTable(0, 0) == readTable(0, 2) && readTable(0, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(0, 0) == readTable(1, 1) && readTable(0, 0) == readTable(2, 2) && readTable(0, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(0, 0) == readTable(1, 0) && readTable(0, 0) == readTable(2, 0) && readTable(0, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(1, 0) == readTable(1, 1) && readTable(1, 0) == readTable(1, 2) && readTable(1, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(2, 0) == readTable(1, 1) && readTable(2, 0) == readTable(0, 2) && readTable(2, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(2, 0) == readTable(2, 1) && readTable(2, 0) == readTable(2, 2) && readTable(2, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(0, 0) == readTable(0, 1) && readTable(0, 1) == readTable(0, 2) && readTable(0, 0) != ' ')
+            {
+                isOver = true;
+            }
+
+            if (readTable(0, 2) == readTable(1, 2) && readTable(0, 2) == readTable(2, 2) && readTable(0, 2) != ' ')
+            {
+                isOver = true;
+            }
+
+            return isOver;
+        }
+
+        public void assignPlayers(string x, string y)
+        {
+            if (Players.Next(1, 3) == 1)
+            {
+                player1 = x;
+                player2 = y;
+            }
+
+            else
+            {
+                player1 = y;
+                player2 = x;
+            }
+        }
+
+        public void getPlayers(out string one, out string two)
+        {
+            one = player1;
+            two = player2;
+        }
+
+        public bool checkTable(int x, int y)
+        {
+            bool isAvailable = false;
+            if (playTable[x, y] == ' ')
+            {
+                isAvailable = true;
+            }
+
+            return isAvailable;
+        }
+    }
+}
+
---
+++ TicTacToe/TicTacToeUI/TicTacToeUIL.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeBLL;
+using Utility_Classes;
+
+namespace TicTacToeUI
+{
+    class TicTacToeUIL
+    {
+        public static void DisplayBoard(GameBoard game)
+        {
+            Console.WriteLine($"{game.readTable(0, 0)}|{game.readTable(0, 1)}|{game.readTable(0, 2)}");
+            Console.WriteLine("------");
+            Console.WriteLine($"{game.readTable(1, 0)}|{game.readTable(1, 1)}|{game.readTable(1, 2)}");
+            Console.WriteLine("------");
+            Console.WriteLine($"{game.readTable(2, 0)}|{game.readTable(2, 1)}|{game.readTable(2, 2)}");
+        }
+        static void Main(string[] args)
+        {
+            bool willParse = false;
+            string answer = null;
+            int choice;
+            bool willQuit = false;
+            bool isAvailable = false;
+            bool isWinner = false;
+            bool playAgain = true;
+            string player1;
+            string player2;
+            string winner = "no one";
+            GameBoard game = new GameBoard();
+
+            while (playAgain)
+            {
+                while (!willQuit)
+                {
+                    //Begin play and assign names.
+                    game.SetBoard();
+                    Console.WriteLine("Hello! Welcome to Tic-Tac-Toe! Please enter the name for Player 1");
+                    player1 = Console.ReadLine();
+                    Console.WriteLine("Please enter the name for Player 2");
+                    player2 = Console.ReadLine();
+
+                    //Shuffle names.
+                    game.assignPlayers(player1, player2);
+
+                    //Return names.
+                    game.getPlayers(out player1, out player2);
+
+                    //Display board.
+                    DisplayBoard(game);
+
+                    //Exit condition.
+                    while (!willQuit && !isWinner)
+                    {
+                        //Turn.
+                        //Player1
+                        Console.WriteLine($"{player1}, you are placing \"O\". Please enter your choice for location or \"Q\" for quit.");
+                        Console.WriteLine("You can choose from the following locations:");
+                        Console.WriteLine("1,2,3");
+                        Console.WriteLine("4,5,6");
+                        Console.WriteLine("7,8,9");
+                        //Validate and prompt until empty.
+                        answer = Console.ReadLine();
+                        if (answer == "Q")
+                        {
+                            willQuit = true;
+                        }
+                        else
+                        {
+                            willParse = Int32.TryParse(answer, out choice);
+                            if (willParse && choice > 0 && choice < 10)
+                            {
+                                isAvailable = game.AssignBoard(choice, "player1");
+                                while (!isAvailable)
+                                {
+                                    isAvailable = game.AssignBoard(GetInput.ForInt(1, 9), "player1");
+                                }
+                            }
+
+                            else
+                                while (!isAvailable)
+                                {
+                                    Console.WriteLine("Please enter an empty location.");
+                                    isAvailable = game.AssignBoard(GetInput.ForInt(1, 9), "player1");
+                                }
+
+                        }
+
+                        isAvailable = false;
+                        //Display result.
+                        DisplayBoard(game);
+                        //Check winner.
+                        if (game.turnCounter == 9)
+                        {
+                            isWinner = true;
+                            winner = "no one";
+                        }
+                        else
+                        {
+                            isWinner = game.checkWin();
+                            if (isWinner)
+                            {
+                                winner = player1;
+                            }
+                        }
+
+                        if (!willQuit && !isWinner)
+                        {
+                            answer = null;
+                            //Player2
+                            Console.WriteLine($"{player2}, you are placing \"X\". Please enter your choice for location or hit \"Q\" to quit.");
+                            Console.WriteLine("You can choose from the following locations:");
+                            Console.WriteLine("1,2,3");
+                            Console.WriteLine("4,5,6");
+                            Console.WriteLine("7,8,9");
+                            //Validate and prompt until empty.
+                            answer = Console.ReadLine();
+                            if (answer == "Q")
+                            {
+                                willQuit = true;
+                            }
+                            else
+                            {
+                                willParse = Int32.TryParse(answer, out choice);
+                                if (willParse && choice > 0 && choice < 10)
+                                {
+                                    isAvailable = game.AssignBoard(choice, "player1");
+                                    while (!isAvailable)
+                                    {
+                                        isAvailable = game.AssignBoard(GetInput.ForInt(1, 9), "player2");
+                                    }
+                                }
+
+                                else
+                                    while (!isAvailable)
+                                    {
+                                        Console.WriteLine("Please enter an empty location.");
+                                        isAvailable = game.AssignBoard(GetInput.ForInt(1, 9), "player2");
+                                    }
+
+                            }
+                            isAvailable = false;
+
+                            //Display result.
+                            DisplayBoard(game);
+                            //Check winner.
+                            if (game.turnCounter == 9)
+                            {
+                                winner = "no one";
+                                isWinner = true;
+                            }
+
+                            else
+                            {
+                                isWinner = game.checkWin();
+                                if (isWinner)
+                                {
+                                    winner = player2;
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine($"The game is over and {winner} has won!");
+
+                    Console.WriteLine("Would you like to play again? Yes or No.");
+                    answer = Console.ReadLine();
+                    while (answer != "Yes" && answer != "No")
+                    {
+                        Console.WriteLine("Would you like to play again? Yes or No.");
+                        answer = Console.ReadLine();
+                    }
+                    if (answer == "Yes")
+                    {
+                        playAgain = true;
+                        willQuit = false;
+                        isWinner = false;
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
