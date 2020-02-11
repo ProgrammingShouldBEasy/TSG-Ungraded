c955d07c87a9cecbff3fcb6e18c491ac4e81bd40
Cain <caintaylor2015@gmail.com>
2020-02-11 11:19:49 -0600

    Exercise with DateTimes and List<T>

---
+++ Test/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool newBool = true;
+            newBool = !newBool;
+            Console.ReadLine();
+        }
+    }
+}
