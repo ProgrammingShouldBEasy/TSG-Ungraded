f351d275777d3946490a34c6143fb52293c49c9f
Cain <caintaylor2015@gmail.com>
2020-03-09 14:35:52 -0500

    Refactored from 2NF to 3NF, queries are left.

--- HotelSQL/CainTaylor-HotelDB.sql
+++ HotelSQL/CainTaylor-HotelDB.sql
@@ -9,25 +9,37 @@ USE [Hotel]
 
 CREATE TABLE [Rooms]
 ([Room Number] INT Primary Key,
-[Type] nvarchar(50) NOT NULL,
-[Microwave] bit NOT NULL,
-[Jacuzzi]  bit NOT NULL,
-[Fridge] bit NOT NULL,
-[Oven] bit NOT NULL,
-[ADA Accessible] bit NOT NULL,
-[Standard Occupancy] INT NOT NULL,
-[Maximum Occupancy] INT NOT NULL,
-[Price] decimal(15,2) NOT NULL,
-[Extra Person Cost] decimal(15,2) NULL)
+[Size] nvarchar(50) NOT NULL,
+[ADA] bit NOT NULL)
 
-CREATE TABLE [Reservations]
-([ReservationID] INT Primary Key Identity(1,1),
-[Room Number] INT NOT NULL,
+CREATE TABLE [Room Type]
+([Size] nvarchar(50) PRIMARY KEY ,
+[Base Price] decimal(15,2) NOT NULL,
+[Extra Person Charge] decimal(15,2) NULL,
+[Standard Occupancy] INT NOT NULL,
+[Maximum Occupancy] INT NOT NULL)
+
+CREATE TABLE [Room to Amenities]
+([Room Number] INT,
+[AmenityID] INT,
+PRIMARY KEY ([Room Number], [AmenityID]))
+
+CREATE TABLE [Room to Reservation]
+([Room Number] INT,
+[ReservationID] INT,
 [Adults] INT NOT NULL,
 [Children] INT NOT NULL,
+PRIMARY KEY ([Room Number], [ReservationID]))
+
+CREATE TABLE [Amenities]
+([AmenityID] INT PRIMARY KEY IDENTITY(1,1),
+[Amenity Name] nvarchar(50) NOT NULL,
+[Charge] decimal(15,2) NOT NULL)
+
+CREATE TABLE [Reservation]
+([ReservationID] INT Primary Key Identity(1,1),
 [Start Date] Date NOT NULL,
 [End Date] Date NOT NULL,
-[Cost] decimal(15,2) NOT NULL,
 [GuestID] INT NOT NULL)
 
 CREATE TABLE [Guests]
@@ -39,17 +51,33 @@ CREATE TABLE [Guests]
 [Zip] INT NOT NULL,
 [Phone] nvarchar(50) NOT NULL)
 
-ALTER TABLE [Reservations]
+ALTER TABLE [Room to Amenities]
 ADD CONSTRAINT fk_RoomNumber
 FOREIGN KEY ([Room Number])
-REFERENCES Rooms([Room Number])
-ON DELETE CASCADE
-ON UPDATE CASCADE
+REFERENCES [Rooms]([Room Number])
 
-ALTER TABLE [Reservations]
+ALTER TABLE [Room to Amenities]
+ADD CONSTRAINT fk_AmenityID
+FOREIGN KEY ([AmenityID])
+REFERENCES [Amenities]([AmenityID])
+
+ALTER TABLE [Rooms]
+ADD CONSTRAINT fk_RoomSize
+FOREIGN KEY ([Size])
+REFERENCES [Room Type]([Size])
+
+ALTER TABLE [Room to Reservation]
+ADD CONSTRAINT fk_RoomNumbertoReservation
+FOREIGN KEY ([Room Number])
+REFERENCES [Rooms]([Room Number])
+
+ALTER TABLE [Room to Reservation]
+ADD CONSTRAINT fk_ReservationID
+FOREIGN KEY ([ReservationID])
+REFERENCES [Reservation]([ReservationID])
+
+ALTER TABLE [Reservation]
 ADD CONSTRAINT fk_GuestID
 FOREIGN KEY ([GuestID])
-REFERENCES Guests([GuestID])
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+REFERENCES [Guests]([GuestID])
 GO
