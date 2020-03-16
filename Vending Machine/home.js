7e433c169909a94b4b29d007d3b936e3fa089244
Cain <caintaylor2015@gmail.com>
2020-03-16 13:33:30 -0500

    Vending Machine GET works and add money functions work.

---
+++ Vending Machine/home.js
@@ -0,0 +1,42 @@
+$(document).ready(function (){
+    $.ajax({
+        type: "GET",
+        url: "http://tsg-vending.herokuapp.com/items",
+        success: function(itemArray) {
+            $.each(itemArray, function(index, item) {
+                $("#menu").append(
+                "<button type='button'class='menu-selections' id="
+                +item.id+"><div class='item-id'>"+item.id
+                +"</div><br>"+item.name
+                +"<br>$"+item.price.toFixed(2)
+                +"<br><br>Quantity Left: "+item.quantity
+                +"</button>").text();
+            })
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    })
+});
+
+var sum = parseFloat("0");
+
+$("#add-Dollar").click(function(){
+    sum += parseFloat("1");
+    $("#mAmount").val(sum.toFixed(2));
+});
+
+$("#add-Quarter").click(function(){
+    sum += parseFloat(".25");
+    $("#mAmount").val(sum.toFixed(2));
+});
+
+$("#add-Dime").click(function(){
+    sum += parseFloat(".1");
+    $("#mAmount").val(sum.toFixed(2));
+});
+
+$("#add-Nickel").click(function(){
+    sum += parseFloat(".05");
+    $("#mAmount").val(sum.toFixed(2));
+});
