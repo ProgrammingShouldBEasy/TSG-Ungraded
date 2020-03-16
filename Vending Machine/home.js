716e8dc3c9d1161d4ab0c18a4de2da7f6dbdfd96
Cain <caintaylor2015@gmail.com>
2020-03-16 16:19:36 -0500

    DVD Library and Vending Machine are both done.

--- Vending Machine/home.js
+++ Vending Machine/home.js
@@ -1,4 +1,9 @@
 $(document).ready(function (){
+    load();
+});
+
+function load() {
+    $("#menu").empty();
     $.ajax({
         type: "GET",
         url: "http://tsg-vending.herokuapp.com/items",
@@ -17,7 +22,7 @@ $(document).ready(function (){
             alert(xhr.responseText);
         }
     })
-});
+};
 
 var sum = parseFloat("0");
 
@@ -48,3 +53,72 @@ $("#add-Nickel").click(function(){
 function select(x) {
     $("#item-id").val($(x).attr('id'));
 };
+
+$("#submit-purchase").click(function() {
+    if ($("#item-id").val() == "")
+    {
+        $("#message").val("Please make a selection");
+        setTimeout(function(){
+           load();
+        }, 100)
+    }
+    else {
+        $.ajax({
+            type: "POST",
+            url: "http://tsg-vending.herokuapp.com/money/"+ $("#mAmount").val()
+            +"/item/"
+            +$("#item-id").val(),
+            success: function(change) {
+                $("#change").val(change.quarters+" quarters, "+
+                change.dimes+" dimes, "+
+                change.nickels+" nickels, "+
+                change.pennies+" pennies");
+                $("#message").val("Thank You!!!");
+                $("#mAmount").val(0.00);
+                setTimeout(function(){
+                    $("#menu").empty();
+                    $.ajax({
+                        type: "GET",
+                        url: "http://tsg-vending.herokuapp.com/items",
+                        success: function(itemArray) {
+                            $.each(itemArray, function(index, item) {
+                                $("#menu").append(
+                                "<button type='button'class='menu-selections' id="
+                                +item.id+" onclick='select(this)'><div class='item-id'>"+item.id
+                                +"</div><br>"+item.name
+                                +"<br>$"+item.price.toFixed(2)
+                                +"<br><br>Quantity Left: "+item.quantity
+                                +"</button>").text();
+                            })
+                        },
+                        error: function (xhr, status, error) {
+                            alert(xhr.responseText);
+                        }
+                    })
+                }, 100)
+                },
+            error: function (error) {
+                $("#message").val(JSON.parse(error.responseText).message);
+                load();
+            }
+        })
+    }
+});
+
+$("#change-return").click(function() {
+    if($("#mAmount").val() == 0.00) {
+        $("#change-return").val(0.00)
+    }
+
+    else {
+        var total = $("#mAmount").val();
+        var quarters = Math.floor((total/.25));
+        total = (total - quarters*.25).toFixed(2);
+        var dimes = Math.floor((total/.10));
+        total= (total-dimes%.1).toFixed(2);
+        var nickels = Math.floor((total/.05));
+        $("#change").val(quarters + " quarters, "+
+        dimes + " dimes, "+
+        nickels + " nickels");
+    }
+});
