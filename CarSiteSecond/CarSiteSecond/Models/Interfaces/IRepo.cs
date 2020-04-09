5cb12adb03edb0e7cf7f2705ae9a49c92385ed05
Cain <caintaylor2015@gmail.com>
2020-04-08 21:07:18 -0500

    IRepo interface is fleshed out with bare minimum Get methods for each DTO using the Request/Response structure. Not sure exactly how to build that request/response structure but at least I have them in the interface. Will need to add CRUD methods for each table/DTO eventually.

--- CarSiteSecond/CarSiteSecond/Models/Interfaces/IRepo.cs
+++ CarSiteSecond/CarSiteSecond/Models/Interfaces/IRepo.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarSiteSecond.Models.DTOs;
+using CarSiteSecond.Models.Requests;
+using CarSiteSecond.Models.Responses;
 
 namespace CarSiteSecond.Models.Interfaces
 {
     public interface IRepo
     {
+        CarResponse GetCars(CarRequest carRequest);
+        ContactResponse GetContacts(ContactRequest contactRequest);
+        MakeResponse GetMakes(MakeRequest makeRequest);
+        ModelResponse GetModels(ModelRequest modelRequest);
+        SaleResponse GetSales(SaleRequest saleRequest);
+        UserResponse GetUsers(UserRequest userRequest);
     }
 }
